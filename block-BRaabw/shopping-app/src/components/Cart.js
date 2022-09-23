af7c2dea101ffe5184b1877441f98824d8e10a0b
lovekushrajput <lovekushrazput143@gmail.com>
2022-09-23 19:40:08 +0530

    'add-localstorage-support-and-life-cycle-method'

--- block-BRaabw/shopping-app/src/components/Cart.js
+++ block-BRaabw/shopping-app/src/components/Cart.js
@@ -1,63 +1,92 @@
-import React from "react"
+import React from 'react'
+import CartItems from './CartItems'
 
-function Cart(props) {
+class Cart extends React.Component {
+    constructor() {
+        super()
+        this.state = {
+            isOpen: false
+        }
+    }
 
-    let { product, handleInc, handleDec, handleDel } = props
+    open = () => {
+        this.setState({ isOpen: true })
+    }
 
+    close = () => {
+        this.setState({ isOpen: false })
+    }
+
+    render() {
+        let { isOpen } = this.state
+        let { cartItems, incrementQuantity, decrementQuantity, deletetItem } = this.props
+
+        let totalQuantity = cartItems.reduce((acc, cv) => {
+            acc = acc + cv.quantity
+            return acc
+        }, 0)
+
+        let totalAmount = cartItems.reduce((acc, cv) => {
+            acc = acc + cv.price * cv.quantity
+            return acc
+        }, 0)
+
+        if (!isOpen) {
+            return <ClosedCart open={this.open} cartItems={cartItems} totalQuantity={totalQuantity} />
+        }
+        return (
+            <>
+                <button className='close flex justify-center align-center curser' onClick={this.close}>✖</button>
+                <div className='cart padding'>
+                    <div className='flex justify-center align-center'>
+                        <figure>
+                            <img src='/static/bag-icon.png' alt='hh' />
+                        </figure>
+
+                        <span className='popup popup---cart weight-500'>
+                            {totalQuantity}
+                        </span>
+
+                        <h2 className='font-1   margin-left'>Cart</h2>
+                    </div>
+
+                    <hr />
+                    {cartItems.map((item) => <CartItems
+                        item={item}
+                        decrementQuantity={decrementQuantity}
+                        incrementQuantity={incrementQuantity}
+                        deletetItem={deletetItem}
+                    />)}
+
+
+                    <div className="flex justify-space width-100">
+
+                        {totalAmount > 0 ?
+                            <> <h2>SUBTOTAL</h2> <p className='weight-500'>$ {Math.floor(totalAmount)}.00 </p> </>
+                            : ''}
+                    </div>
+
+                    {totalAmount > 0 ? <div className='flex justify-center flex-column'>
+                        <button className='checkout' onClick={() => alert('Thanks for perchasing with shopping cart 🤟😎')}>checkout</button>
+                    </div> : ''}
+                </div>
+            </>
+        )
+    }
+}
+
+
+function ClosedCart(props) {
     return (
         <>
-            <div key={product.id} className='flex margin-botttom'>
-
-                <figure className='cart-img'>
-                    <img src={'/static/products/' + product.sku + '_2.jpg'} alt={product.sku} />
+            <div className='icon'>
+                <figure onClick={props.open}>
+                    <img src='/static/bag-icon.png' alt='hh' />
                 </figure>
-
-                <div className=" flex justify-even width-100">
-
-                    <div className="line-2" >
-                        <h2>
-                            {product.title}
-                        </h2>
-                        <p
-                            className="grey">{product.style}
-                        </p>
-                        <span
-                            className="grey">print Quantity: {product.quantity}
-                        </span>
-                    </div>
-
-                    <div className="flex flex-column line-2 left">
-                        <p
-                            className="yellow weight-500">{product.currencyFormat} <br
-                            />{product.price * product.quantity}
-                        </p>
-                        <span
-                            onClick={(e) => handleInc(e, product)}
-                            className='curser'>➕
-                        </span>
-                        <span
-                            onClick={(e) => handleDec(e, product)}
-                            className='curser'>➖
-                        </span>
-                    </div>
-
-                    <div>
-                        <span
-                            className="black curser"
-                            onClick={(e) => handleDel(e, product)}>✖
-                        </span>
-                    </div>
-
-                </div>
-
-
             </div>
-            {/* <hr/> */}
+            <span className='popup popup---main weight-500'>{props.totalQuantity}</span>
         </>
-
-
     )
-
 }
 
 export default Cart
